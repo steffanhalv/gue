7255657ba477577e6f3cfbcb34ebff4a29deaefe
steffanhalv <steffanhalv@gmail.com>
2019-12-09 12:32:10 +0100

    Cleaned project

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.prototype.store = storage
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
